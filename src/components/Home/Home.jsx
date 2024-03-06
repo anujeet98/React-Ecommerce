d0d9d8bf8f5918a006354820c9a5f7671374c062
Anujeet Swain <anujeet98@gmail.com>
2024-03-07 03:56:45 +0530

    react-fetch demo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Footer from "../AppBody/Footer/Footer";
 import Header from "../AppBody/Header/Header";
 import { Button, Container, Row, Table } from "react-bootstrap";
@@ -7,6 +7,26 @@ import './Home.css';
 
 
 const Home = () => {
+    const [movies, setMovies] = useState([]);
+    const fetchMoviesHandler = async() => {
+        try{
+            const resp = await fetch('https://swapi.dev/api/films/');
+            const data = await resp.json();
+            const transformedMovies = data.results.map(movie => {
+                return {
+                    id: movie.episode_id,
+                    title: movie.title,
+                    openingText: movie.opening_crawl,
+                    releaseDate: movie.release_date,
+                }
+            })
+            setMovies(transformedMovies);
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+
 
     const tours = [{date:'JUL16', location:'DETROIT, MI', place:'DTE ENERGY MUSIC THEATRE'},{date:'JUL19', location:'TORONTO,ON', place:'BUDWEISER STAGE'},{date:'JUL 22', location:'BRISTOW, VA', place:'JIGGY LUBE LIVE'},{date:'JUL 29', location:'PHOENIX, AZ', place:'AK-CHIN PAVILION'},{date:'AUG 2', location:'LAS VEGAS, NV', place:'T-MOBILE ARENA'},{date:'AUG 7', location:'CONCORD, CA', place:'CONCORD PAVILION'}]
     
@@ -37,6 +57,17 @@ const Home = () => {
                     </Table>
                 </Row>
             </Container>
+
+            <Container>
+                <section><Button onClick={fetchMoviesHandler}>Fetch Movies</Button></section>
+                <section>
+                    <ul>
+                        {console.log(movies)}
+                    </ul>
+                </section>
+            </Container>
+
+
             <Footer/>
         </Fragment>
     );
